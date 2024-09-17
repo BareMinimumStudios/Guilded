e1b8f74bca198ab256148e2213cad8491806d58f
db318 <db318330@gmail.com>
2024-09-17 15:55:23 +1200

    It begins

---
+++ src/main/java/keno/net/guilded_parties/GuildedPartiesDataGenerator.java
@@ -0,0 +1,11 @@
+package keno.net.guilded_parties;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GuildedPartiesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
