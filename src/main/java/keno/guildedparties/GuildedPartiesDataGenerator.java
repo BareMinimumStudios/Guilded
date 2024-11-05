d59b6859abc07a529d33726c0bfdca5ee9924538
XeroTheProtogen <db318330@gmail.com>
2024-11-06 00:35:26 +1300

    Begin rewrite

--- src/main/java/keno/guildedparties/GuildedPartiesDataGenerator.java
+++ src/main/java/keno/guildedparties/GuildedPartiesDataGenerator.java
@@ -1,4 +1,4 @@
-package keno.net.guilded_parties;
+package keno.guildedparties;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
--- src/main/java/keno/net/guilded_parties/utils/PacketIds.java
+++
@@ -1,7 +0,0 @@
-package keno.net.guilded_parties.utils;
-
-import net.minecraft.util.Identifier;
-
-public class PacketIds {
-    public static final Identifier VIEW_GUILDS = IdUtils.modLoc("packet/view_guilds");
-}
