27b7af0b391a23594fa4061bc1a77e6300105033
XeroTheProtogen <db318330@gmail.com>
2024-11-06 11:53:31 +1300

    Finish persistent StateSaverAndLoader

---
+++ src/main/java/keno/guildedparties/server/StateSaverAndLoader.java
@@ -0,0 +1,60 @@
+package keno.guildedparties.server;
+
+import keno.guildedparties.GuildedParties;
+import keno.guildedparties.data.guilds.Guild;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.PersistentState;
+import net.minecraft.world.PersistentStateManager;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class StateSaverAndLoader extends PersistentState {
+    public HashMap<String, Guild> guilds = new HashMap<>();
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registries) {
+        StringBuilder keySet = new StringBuilder();
+        List<String> keys = new ArrayList<>(guilds.keySet());
+        for (String key : keys) {
+            NbtElement guild = Guild.codec.encodeStart(NbtOps.INSTANCE, guilds.get(key)).resultOrPartial(GuildedParties.LOGGER::error).orElseThrow();
+
+            nbt.put(key, guild);
+            if (!key.equals(keys.getLast())) {
+                keySet.append(key);
+                keySet.append(",");
+            }
+        }
+        nbt.putString("keySet", "" + keySet);
+        return nbt;
+    }
+
+    public static StateSaverAndLoader createFromNbt(NbtCompound tag, RegistryWrapper.WrapperLookup lookup) {
+        StateSaverAndLoader saverAndLoader = new StateSaverAndLoader();
+        String keySet = tag.getString("keySet");
+        String[] keys = keySet.split(",");
+        for (String key : keys) {
+            Guild guild = Guild.codec.parse(NbtOps.INSTANCE, tag.get(key)).resultOrPartial(GuildedParties.LOGGER::error).orElseThrow();
+            saverAndLoader.guilds.put(key, guild);
+        }
+        return saverAndLoader;
+    }
+
+    private static Type<StateSaverAndLoader> type = new Type<>(
+            StateSaverAndLoader::new,
+            StateSaverAndLoader::createFromNbt,
+            null);
+
+    public static StateSaverAndLoader getStateFromServer(MinecraftServer server) {
+        PersistentStateManager manager = server.getWorld(World.OVERWORLD).getPersistentStateManager();
+        StateSaverAndLoader saverAndLoader = manager.getOrCreate(type, GuildedParties.MOD_ID);
+        saverAndLoader.markDirty();
+        return saverAndLoader;
+    }
+}
