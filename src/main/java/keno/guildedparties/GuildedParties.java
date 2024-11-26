deda081833783b52fccbfd97043f4c5f5a1d4d4a
KenopsicManiac <106413002+XeroTheProtogen@users.noreply.github.com>
2024-11-27 01:50:21 +1300

    Update GuildedParties.java

--- src/main/java/keno/guildedparties/GuildedParties.java
+++ src/main/java/keno/guildedparties/GuildedParties.java
@@ -42,8 +42,6 @@ import java.util.HashMap;
 public class GuildedParties implements ModInitializer {
 	public static final String MOD_ID = "guildedparties";
 
-
-
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
