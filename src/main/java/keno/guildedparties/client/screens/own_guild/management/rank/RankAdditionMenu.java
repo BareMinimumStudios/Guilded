44c985db685da5408c7724f12abd769e639ee4e5
KenopsicManiac <db318330@gmail.com>
2024-12-06 17:00:31 +1300

    Why did I even commit this
    This reverts commit b3c174ca7c40fc6fa5a4318fc3f40a31518fb49e.

--- src/main/java/keno/guildedparties/client/screens/own_guild/management/rank/RankAdditionMenu.java
+++ src/main/java/keno/guildedparties/client/screens/own_guild/management/rank/RankAdditionMenu.java
@@ -61,7 +61,6 @@ public class RankAdditionMenu extends BaseUIModelScreen<FlowLayout> {
                     if (!this.hasSliderBeenMoved) {
                         this.rankPriority = MathUtil.denormalizeValues(this.rankPriority, 1d, 50d);
                     }
-
                     GPNetworking.GP_CHANNEL.clientHandle().send(new AddRankPacket(guildName, rankName, (int) rankPriority));
                     this.client.setScreen(null);
                 });
