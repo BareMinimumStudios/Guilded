7a63d15d5bd6a3dcce66724d6944537efaac43da
KenopsicManiac <db318330@gmail.com>
2024-12-10 12:52:22 +1300

    Publishing task

--- src/main/java/keno/guildedparties/config/GPServerConfigModel.java
+++ src/main/java/keno/guildedparties/config/GPServerConfigModel.java
@@ -1,7 +1,9 @@
 package keno.guildedparties.config;
 
+import io.wispforest.owo.config.Option;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.RestartRequired;
+import io.wispforest.owo.config.annotation.Sync;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
@@ -9,5 +11,6 @@ import net.fabricmc.api.Environment;
 @Config(name = "gp-server-config", wrapperName = "GPServerConfig")
 public class GPServerConfigModel {
     @RestartRequired
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public boolean enableServerCommands = false;
 }
