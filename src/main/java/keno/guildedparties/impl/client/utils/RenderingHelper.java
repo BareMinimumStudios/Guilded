4616a721e64219dc4a3d1360b03d0134c3fe9a9d
KenopsicManiac <db318330@gmail.com>
2025-10-22 23:03:36 +1300

    Add helper method to get buffers

--- src/main/java/keno/guildedparties/impl/client/utils/RenderingHelper.java
+++ src/main/java/keno/guildedparties/impl/client/utils/RenderingHelper.java
@@ -1,6 +1,5 @@
 package keno.guildedparties.impl.client.utils;
 
-import io.wispforest.owo.ui.core.OwoUIDrawContext;
 import keno.guildedparties.mixin.client.DrawContextInvoker;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -15,14 +14,19 @@ public class RenderingHelper {
      * {@link DrawContext#fillGradient(int, int, int, int, int, int)} but sideways
      */
     public static void fillSidewaysGradient(DrawContext context, int startX, int startY, int endX, int endY, int colorStart, int colorEnd) {
-        filLSidewaysGradient(context, ((DrawContextInvoker)context).getVertexConsumers().getBuffer(RenderLayer.getGui()), startX, startY, endX, endY, colorStart, colorEnd);
+        filLSidewaysGradient(context, startX, startY, endX, endY, colorStart, colorEnd);
     }
 
-    public static void filLSidewaysGradient(DrawContext context, VertexConsumer vertexConsumer, int startX, int startY, int endX, int endY, int colorStart, int colorEnd) {
+    public static void filLSidewaysGradient(DrawContext context, int startX, int startY, int endX, int endY, int colorStart, int colorEnd) {
+        VertexConsumer vertexConsumer = getBufferFromContext(context, RenderLayer.getGui());
         Matrix4f matrix4f = context.getMatrices().peek().getPositionMatrix();
         vertexConsumer.vertex(matrix4f, (float)startX, (float)startY, 0f).color(colorStart);
         vertexConsumer.vertex(matrix4f, (float)startX, (float)endY, 0f).color(colorStart);
         vertexConsumer.vertex(matrix4f, (float)endX, (float)endY, 0f).color(colorEnd);
         vertexConsumer.vertex(matrix4f, (float)endX, (float)startY, 0f).color(colorEnd);
+    }
+
+    public static VertexConsumer getBufferFromContext(DrawContext context, RenderLayer layer) {
+        return ((DrawContextInvoker)context).getVertexConsumers().getBuffer(layer);
     }
 }
